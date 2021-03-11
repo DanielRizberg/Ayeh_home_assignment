30e263347776323e626a05f39f0277e4fb2d4a34
daniel <meltsev1@gmail.com>
2021-03-11 19:54:21 +0200

    new stuff

--- Ayeh_home_assignment/ClientApp/src/app/pages/grid-module/data-grid/data-grid.component.ts
+++ Ayeh_home_assignment/ClientApp/src/app/pages/grid-module/data-grid/data-grid.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { post } from 'src/app/models/post';
+import { filterOp } from 'src/app/models/filterOp';
 
 @Component({
   selector: 'app-data-grid',
   templateUrl: './data-grid.component.html',
-  styleUrls: ['./data-grid.component.scss']
+  styleUrls: ['./data-grid.component.scss'],
 })
 export class DataGridComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
-  constructor(private apiService:ApiService) { }
-
-  data:post[];
+  data: post[];
   ngOnInit(): void {
+    let request = {
+      sortDir: 'asc',
+      searchProp: filterOp.none,
+      searchVal: '',
+      sortProp: filterOp.none,
+    };
+    this.apiService.getData(request).subscribe((x) => (this.data = x.posts));
   }
-
 }
