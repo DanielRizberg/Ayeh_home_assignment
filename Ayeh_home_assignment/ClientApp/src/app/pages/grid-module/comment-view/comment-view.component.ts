b632ce503702d92ebd685fe8501fe3223261ae52
daniel <meltsev1@gmail.com>
2021-03-14 14:42:27 +0200

    changes

--- Ayeh_home_assignment/ClientApp/src/app/pages/grid-module/comment-view/comment-view.component.ts
+++ Ayeh_home_assignment/ClientApp/src/app/pages/grid-module/comment-view/comment-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-comment-view',
@@ -8,15 +9,16 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./comment-view.component.scss'],
 })
 export class CommentViewComponent implements OnInit, OnDestroy {
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute,private apiService:ApiService) {}
   ngOnDestroy(): void {
     this.subs.unsubscribe();
   }
-
+postId:number;
   ngOnInit(): void {
     this.subs = this.activatedRoute.params.subscribe((x) => {
       let post_id = x['post_id'];
-      //console.log(post_id);
+     this.postId=post_id;
+     this.apiService.getCommentsById(post_id).subscribe(x=>{});
     });
   }
   subs: Subscription;
